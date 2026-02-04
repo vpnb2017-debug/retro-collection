3e00f36e64b032caed301515e5880426e50a5f3f
vpnb2017-debug <vpnb2017-debug@users.noreply.github.com>
2026-02-04 17:17:24 +0000

    v102: Correção Crítica Gist ID 🐞🔧 (Suporte para Maiúsculas/Minúsculas)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'retro-collection-v101';
+const CACHE_NAME = 'retro-collection-v102';
 const ASSETS = [
     './',
     './index.html',
@@ -19,7 +19,7 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('SW: Pre-caching v101');
+            console.log('SW: Pre-caching v102');
             return cache.addAll(ASSETS);
         })
     );
